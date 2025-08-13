f1af6865492885dd116579697298d31a64eff4c8
Andrew <bakarekeandrew@gmail.com>
2025-08-13 20:39:46 +0200

    vanlife project setup

---
+++ routers/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+       <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+
+       </Routes>
+    </>
+  )
+}
+
+export default App
